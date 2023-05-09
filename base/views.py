2ced74db11abce0f1755d4a3ec82f234340a0785
Rukaiya Amin <rukaiyaamin36@gmail.com>
2023-05-09 07:33:58 +0600

    1:44 create-room view

--- base/views.py
+++ base/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Room
 
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
